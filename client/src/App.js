6750a4f8753b171af50cbd0023b0e6087d805916
Peter Liu <peter9342@gmail.com>
2025-04-11 01:12:01 -0700

    clean up unused files

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LoginPage from "./pages/login";
 import PreferencesPage from "./pages/preferences";
-import SignupPage from "./pages/signup";
 import DashboardPage from "./pages/dashbord";
 
 function App() {
