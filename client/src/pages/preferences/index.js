21e9ee058b2a001f2e118c91c971993221c10b01
Peter Liu <124539148+liu-petter@users.noreply.github.com>
2025-04-21 22:39:44 -0700

    cards can handle null coords

--- client/src/pages/preferences/index.js
+++ client/src/pages/preferences/index.js
@@ -7,6 +7,7 @@ function PreferencesPage() {
   const preferences = [
     { category: "Eiffel Tower", coord: { lat: 48.8584, lng: 2.2945 } },
     { category: "Louvre Museum", coord: { lat: 48.8606, lng: 2.3376 } },
+    { category: "Louvre Museum", coord: { lat: null, lng: null } }
   ];
 
   /* other test locations
@@ -34,32 +35,46 @@ function PreferencesPage() {
   return (
     <div className="card-container">
       <div className="card-stack">
-        {preferences.map((preference) =>
-        <TinderCard 
-          className='swipable-card' 
-          key={preference.category} 
-          onSwipe={(dir) => swiped(dir, preference.category)} 
-          onCardLeftScreen={() => outOfFrame(preference.category)}
-          preventSwipe={['up', 'down']}
-        >
-          <div className='card-content'>
-            <APIProvider apiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}>
-              <Map
-                style={{width: '700px', height: '700px', direction: 'absolute', left: '50%', transform: 'translate(-50%, -10%)'}}
-                center={preference.coord}
-                defaultZoom={15}
-                gestureHandling={'none'}
-                zoomControl={false}
-                disableDefaultUI={true}
-              >
-                <Marker position={preference.coord} />
-              </Map>
-            </APIProvider>
-            <h3 className="card-title">{preference.category}</h3>
-          </div>
-        </TinderCard>
-        )}
-        </div> 
+      {preferences.map((preference) => {
+        const coordinates = {
+          lat: preference.coord.lat ?? 0,
+          lng: preference.coord.lng ?? 0,
+        };
+
+        return (
+          <TinderCard 
+            className='swipable-card' 
+            key={preference.category} 
+            onSwipe={(dir) => swiped(dir, preference.category)} 
+            onCardLeftScreen={() => outOfFrame(preference.category)}
+            preventSwipe={['up', 'down']}
+          >
+            <div className='card-content'>
+              <APIProvider apiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}>
+                <Map
+                  style={{width: '700px', height: '700px', direction: 'absolute', left: '50%', transform: 'translate(-50%, -10%)'}}
+                  center={coordinates}
+                  defaultZoom={15}
+                  gestureHandling={'none'}
+                  zoomControl={false}
+                  disableDefaultUI={true}
+                >
+                  <Marker position={coordinates} />
+                </Map>
+              </APIProvider>
+              {preference.coord.lat === null || preference.coord.lng === null ? (
+                <div className="card-title">
+                  <h3>{preference.category}</h3>
+                  <h4>Address not available</h4>
+                </div>
+              ) : (
+                <h3 className="card-title">{preference.category}</h3>
+              )}
+            </div>
+          </TinderCard>
+        )
+      })}
+      </div> 
     </div>
   );
 };
