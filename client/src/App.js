61798a9aa92d68f0bfa1dcda66aac67a8564a806
Peter Liu <124539148+liu-petter@users.noreply.github.com>
2025-04-22 15:19:58 -0700

    various cleanup 2

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import PreferencesPage from "./pages/preferences";
 import DashboardPage from "./pages/dashbord";
 import CreatePlanPage from "./pages/createPlan/CreatePlanPage";
 import SwipePage from "./pages/swipe/SwipePage";
@@ -14,7 +13,6 @@ function App() {
       <div>
         <Routes>
           <Route path="/" element={<DashboardPage />} />
-          <Route path="/preferences" element={<PreferencesPage />} />
           <Route path="/create-plan" element={<CreatePlanPage />} />
           <Route path="/swipe" element={<SwipePage />} />
           <Route path="/map" element={<MapView />} />
